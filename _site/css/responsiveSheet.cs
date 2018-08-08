0a4ccf3142a5024f2fe24310385616d0c8f1345d
Phoenix Perry <contact@phoenixperry.com>
2018-08-08 11:31:46 +0100

    adding site

---
+++ _site/css/responsiveSheet.cs
@@ -0,0 +1,146 @@
+
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen 
+and (min-device-width : 320px) 
+and (max-device-width : 480px) {
+/* Styles */
+	#wrapper{
+	margin-left:0 auto;
+    margin-right:0 auto;
+    width: 100%
+    height:auto;
+} 	
+#header{
+	background: url('images/heroImage.png');
+	-moz-background-size:100% auto;
+	-webkit-background-size:100% auto;
+	background-size: 100% auto; 	
+} 
+.logo{
+	margin-top: 20%; 
+	margin-left: 20%;
+}
+}
+
+/* Smartphones (landscape) ----------- */
+@media only screen 
+and (min-width : 321px) {
+	#wrapper{
+	margin-left:0 auto;
+    margin-right:0 auto;
+    width: 100%
+    height:auto;
+} 	
+#header{
+	background: url('images/heroImage.png');
+	-moz-background-size:100% auto;
+	-webkit-background-size:100% auto;
+	background-size: 100% auto; 	
+} 
+.logo{
+	margin-top: 20%; 
+	margin-left: 20%;
+}
+/* Styles */
+}
+
+/* Smartphones (portrait) ----------- */
+@media only screen 
+and (max-width : 320px) {
+	#wrapper{
+	margin-left:0 auto;
+    margin-right:0 auto;
+    width: 100%
+    height:auto;
+} 	
+#header{
+	background: url('images/heroImage.png');
+	-moz-background-size:100% auto;
+	-webkit-background-size:100% auto;
+	background-size: 100% auto; 	
+} 
+.logo{
+	margin-top: 20%; 
+	margin-left: 20%;
+}
+/* Styles */
+}
+
+/* iPads (portrait and landscape) ----------- */
+@media only screen 
+and (min-device-width : 768px) 
+and (max-device-width : 1024px) {
+
+/* Styles */
+}
+
+/* iPads (landscape) ----------- */
+@media only screen 
+and (min-device-width : 768px) 
+and (max-device-width : 1024px) 
+and (orientation : landscape) {
+	#wrapper{
+	margin-left:0 auto;
+    margin-right:0 auto;
+    width: 100%
+    height:auto;
+} 	
+#header{
+	background: url('images/heroImage.png');
+	-moz-background-size:100% auto;
+	-webkit-background-size:100% 471;
+	background-size: 100% auto; 	
+} 
+.logo{
+	margin-top: 20%; 
+	margin-left: 20%;
+}
+/* Styles */
+}
+
+/* iPads (portrait) ----------- */
+@media only screen 
+and (min-device-width : 768px) 
+and (max-device-width : 1024px) 
+and (orientation : portrait) {
+	#wrapper{
+	margin-left:0 auto;
+    margin-right:0 auto;
+    width: 100%
+    height:auto;
+} 	
+#header{
+	background: url('images/heroImage.png');
+	-moz-background-size:100% 471;
+	-webkit-background-size:100% 471;
+	background-size: 100% 471; 	
+} 
+.logo{
+	margin-top: 20%; 
+	margin-left: 20%;
+}
+/* Styles */
+}
+
+/* Desktops and laptops ----------- */
+@media only screen 
+and (min-width : 1224px) {
+/* Styles */
+}
+
+/* Large screens ----------- */
+@media only screen 
+and (min-width : 1824px) {
+/* Styles */
+}
+
+/* iPhone 4 ----------- */
+@media
+only screen and (-webkit-min-device-pixel-ratio : 1.5),
+only screen and (min-device-pixel-ratio : 1.5) {
+/* Styles */
+}
+
+
+
+
